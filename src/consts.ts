da0219e438ab1a60a7e6454e941384dd75b25b96
Shamim Uthman <164878153+shamimuthman@users.noreply.github.com>
2026-03-24 06:28:32 +0000

    Pending changes exported from your codespace

--- src/consts.ts
+++ src/consts.ts
@@ -1,31 +1,30 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Saral Theme'
-export const SITE_DESCRIPTION =
-	'A simple theme for personal blog sites, created for Astro framework'
+export const SITE_TITLE = 'Uthman.blog'
+export const SITE_DESCRIPTION = 'A outeside look of my inside'
 
 // If you deploy to a subdirectory, set the `WEBSITE_BASE` here.
 // e.g. '/blog' if you are deploying to example.com/blog
 // Leave it as an empty string if you are deploying to a root domain (e.g. example.com)
-export const SITE_BASE = '/saral-theme-astro'
+export const SITE_BASE = './'
 
 export const SocialLinks = [
 	{
 		name: 'GitHub',
-		url: 'https://github.com/yashjawale',
+		url: 'https://github.com/shamimuthman',
 	},
 	{
 		name: 'LinkedIn',
-		url: 'https://www.linkedin.com/in/yashjawale/',
+		url: 'https://www.linkedin.com/in/uthmanshamim',
 	},
 	{
 		name: 'X / Twitter',
-		url: 'https://twitter.com/imyashjawale',
+		url: 'https://twitter.com/uthmanshamim',
 	},
 	{
 		name: 'Instagram',
-		url: 'https://instagram.com/imyashjawale',
+		url: 'https://instagram.com/uthmanshamim',
 	},
 ]
 
