7051979218ec7034606e6f651e8b0e31c57df0fe
Shamim Uthman <164878153+shamimuthman@users.noreply.github.com>
2026-04-04 09:07:26 +0000

    update

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,30 +1,37 @@
-import { getCollection } from 'astro:content';
+import { getCollection } from 'astro:content'
 
 export async function get() {
-  const posts = await getCollection('blog'); // replace 'blog' with your collection name
+	const posts = await getCollection('blog') // your blog collection
 
-  const postUrls = posts.map((post) => `
+	const postUrls = posts
+		.map(
+			(post) => `
     <url>
       <loc>https://uthman-blog.onrender.com/blog/${post.slug}/</loc>
       <lastmod>${post.data.pubDate}</lastmod>
+      <changefreq>monthly</changefreq>
       <priority>0.7</priority>
     </url>
-  `).join('');
+  `
+		)
+		.join('')
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
       <loc>https://uthman-blog.onrender.com/</loc>
+      <changefreq>weekly</changefreq>
       <priority>1.0</priority>
     </url>
     <url>
       <loc>https://uthman-blog.onrender.com/blog/</loc>
+      <changefreq>weekly</changefreq>
       <priority>0.9</priority>
     </url>
     ${postUrls}
-  </urlset>`;
+  </urlset>`
 
-  return new Response(sitemap, {
-    headers: { 'Content-Type': 'application/xml' },
-  });
+	return new Response(sitemap, {
+		headers: { 'Content-Type': 'application/xml' },
+	})
 }
